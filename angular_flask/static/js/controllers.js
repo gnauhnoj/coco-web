84492f64b5c0fc74e9b8ea0bf688d748a99cebe8
Jonathan Huang <jon.hc.huang@gmail.com>
2016-04-06 00:27:15 -0400

    hacked together

--- angular_flask/static/js/controllers.js
+++ angular_flask/static/js/controllers.js
@@ -5,6 +5,12 @@ var IndexController = function($scope, dataStore) {
     // update this shit when i array this
     dataStore.currentImg = data.file_name;
     dataStore.currentData = data;
+    var segmentation = data.segmentation;
+    var w = document.documentElement.clientWidth;
+    var h = document.documentElement.clientHeight;
+
+    // console.log(dataStore.currentImg);
+    // console.log(dataStore.currentData);
     var _img = document.getElementById('id1');
     var newImg = new Image();
 
@@ -12,22 +18,70 @@ var IndexController = function($scope, dataStore) {
     // var w = screen.width * ratio;
     // var h = screen.height * ratio;
 
-    var w = document.documentElement.clientWidth;
-    var h = document.documentElement.clientHeight;
-
     newImg.onload = function() {
         _img.src = newImg.src;
         var height = newImg.height;
         var width = newImg.width;
-        var rat_w = width / w;
-        var rat_h = height / h;
+        var scale_x = w / width;
+        var scale_y = h / height;
+        var dim = (width > height) ? 'x' : 'y';
+        var scale;
 
-        _img.width = w;
+        if (dim === 'x') {
+          scale = scale_x;
+          _img.width = w;
+          _img.height = height * scale;
 
-        var b_canvas = document.getElementById("b");
-        var b_context = b_canvas.getContext("2d");
+        } else {
+          scale = scale_y;
+          _img.width = width * scale;
+          _img.height = h;
+        }
+
+        // console.log(_img.width, _img.height);
+        // _img.width = w;
+        // _img.height = height * scale_x;
+
         console.log(_img.width, _img.height);
-        b_context.fillRect(0, 0, _img.width, _img.height);
+
+        var iarea = document.getElementById('image-area');
+        iarea.setAttribute('height', _img.height);
+        iarea.setAttribute('width', _img.width);
+
+        var newElement = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+        newElement.setAttribute('height', _img.height);
+        newElement.setAttribute('width', _img.width);
+        // newElement.setAttribute('aria-label', 'image area');
+        newElement.style.fill = '#D3D3D3';
+        iarea.appendChild(newElement);
+
+        for (var i=0; i<segmentation.length; i++) {
+          // console.log(segmentation[i]);
+          newElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+          newElement.setAttribute('role', 'img-label');
+          var svg_id = 'svg-' + i;
+          newElement.setAttribute('id', svg_id);
+          newElement.setAttribute('aria-label', segmentation[i].category_name);
+          iarea.appendChild(newElement);
+          sub_svg = document.getElementById(svg_id);
+
+          newElement = document.createElementNS('http://www.w3.org/2000/svg', 'polygon');
+          newElement.style.stroke = '#ffffff';
+          newElement.style['fill-opacity'] = 0.4;
+          var pointStr = [];
+          for (var j = 0; j < segmentation[i].points.length; j++) {
+            var newx = (segmentation[i].points[j][0] * scale);
+            var newy = (segmentation[i].points[j][1] * scale);
+            pointStr.push(newx);
+            pointStr.push(newy);
+          }
+          pointStr = pointStr.join(' ');
+          // console.log(pointStr);
+          newElement.setAttribute('points', pointStr);
+          sub_svg.appendChild(newElement);
+          // console.log(pointStr);
+        }
+        // console.log(_img.width, _img.height);
     };
     newImg.src = '/static/img/' + dataStore.currentImg;
   });
@@ -36,7 +90,7 @@ var IndexController = function($scope, dataStore) {
 var dateIfy = function(arr) {
   for (var i = 0; i < arr.length; i++) {
     for (var j = 0; j < arr[i].length; j++) {
-      var newd = new Date(arr[i][j].replace( /(\d{4})-(\d{2})-(\d{2})/, "$2/$3/$1"));
+      var newd = new Date(arr[i][j].replace( /(\d{4})-(\d{2})-(\d{2})/, '$2/$3/$1'));
       arr[i][j] = newd;
     }
   }
