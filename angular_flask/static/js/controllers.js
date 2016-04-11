e65966617ff0eb2f732ced6427ae16be766013e6
Jonathan Huang <jon.hc.huang@gmail.com>
2016-04-11 00:13:52 -0400

    testing this

--- angular_flask/static/js/controllers.js
+++ angular_flask/static/js/controllers.js
@@ -123,11 +123,12 @@ var ImageController = function($scope, $location, dataStore) {
     categoryStr = categoryStr.join(', ');
     // console.log(categoryStr);
 
-    var button = document.createElement('div');
+    var button = document.createElement('button');
     // TODO: figure out how to deal with annoying text...
     var textNode = document.createTextNode('Hover for All Objects');
     button.appendChild(textNode);
     button.className = 'mdl-button mdl-js-button mdl-button--raised mdl-button--accent mdl-cell mdl-cell--12-col';
+    // button.className = 'large expanded button';
     button.setAttribute('aria-label', categoryStr);
     button.id = 'obj-button';
     componentHandler.upgradeElement(button);
