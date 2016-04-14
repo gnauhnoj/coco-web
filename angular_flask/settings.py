2541514e614e44b82e0f4a185aefa28c45f61424
Jonathan Huang <jon.hc.huang@gmail.com>
2016-04-13 20:10:50 -0400

    Chaging settings for strawman

--- angular_flask/settings.py
+++ angular_flask/settings.py
@@ -2,6 +2,8 @@ import os
 
 MONGO_URL = os.environ.get('MONGOLAB_URI')
 if not MONGO_URL:
+    MONGO_URL = os.environ.get('MONGODB_URI')
+if not MONGO_URL:
     import config
     MONGO_URL = config.MONGO_URL
 DEBUG = True
